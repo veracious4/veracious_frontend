0062d12e7651517562b898c4074189be5b29560c
bhavesh <bkpassi2017@gmail.com>
2022-12-18 17:27:49 +0530

    Added Logo To Website

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import { Nav, NavItem, Navbar, Container } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import {LinkContainer} from 'react-router-bootstrap'
 import {} from 'react-icons/fa';
+import Logo from "../../assets/images/Veracious_Logo.png"
 import './Header.css'
 
 
@@ -19,7 +20,9 @@ class Header extends React.Component{
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="nav_bar_section">
         <Container>
           <LinkContainer to="/">
-          <Navbar.Brand className="navbar__brand">Veracious</Navbar.Brand>
+          <Navbar.Brand className="navbar__brand">
+            <img src={Logo} height="70" width="70" alt="Veracious" /> Veracious  
+          </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
